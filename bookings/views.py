f5ec774c954de07f74ecb12d1c8a90289c93548f
SandyWyper <sandywyper@gmail.com>
2025-08-14 14:37:35 +0100

    Add: implement booking slots retrieval and update index.html layout

--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import BookingTimeSlot
 
 def index(request):
 	return render(request, 'index.html')
+
+def get_bookings_slots(request):
+    slots = BookingTimeSlot.objects.all()
+    return render(request, 'index.html', {'slots': slots})
