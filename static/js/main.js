6af2694878c8faccff442bf1ba468563e2b9714b
SandyWyper <sandywyper@gmail.com>
2025-08-14 14:28:48 +0100

    fix db ref

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,18 @@
 // Modal open/close logic
-window.addEventListener('DOMContentLoaded', function() {
-    var modal = document.getElementById('dateModal');
-    var openBtn = document.getElementById('openModalBtn');
-    var closeBtn = document.getElementById('closeModalBtn');
-    openBtn.onclick = function() {
-        modal.style.display = 'block';
+window.addEventListener("DOMContentLoaded", function () {
+  console.log("DOMContentLoaded");
+  var modal = document.getElementById("dateModal");
+  var openBtn = document.getElementById("openModalBtn");
+  var closeBtn = document.getElementById("closeModalBtn");
+  openBtn.onclick = function () {
+    modal.style.display = "block";
+  };
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
-    closeBtn.onclick = function() {
-        modal.style.display = 'none';
-    }
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    }
+  };
 });
