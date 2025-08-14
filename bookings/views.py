0a09708e5b77889ad6a87be3ee69fcddab6a8a67
SandyWyper <sandywyper@gmail.com>
2025-08-14 14:43:34 +0100

    booking slots rendered on page

--- bookings/views.py
+++ bookings/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from .models import BookingTimeSlot
 
 def index(request):
-	return render(request, 'index.html')
-
-def get_bookings_slots(request):
     slots = BookingTimeSlot.objects.all()
-    return render(request, 'index.html', {'slots': slots})
+    return render(request, 'index.html', {'slots': slots })
