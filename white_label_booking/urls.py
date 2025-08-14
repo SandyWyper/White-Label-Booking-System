17f09744e24379961fd4be257fadebc4a653f4fb
Tom Bragg <tbragg88@hotmail.com>
2025-08-14 13:51:56 +0100

    main page simple

--- white_label_booking/urls.py
+++ white_label_booking/urls.py
@@ -22,5 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include("bookings.urls"), name='index'),
+    path('', include("bookings.urls")),
 ]
