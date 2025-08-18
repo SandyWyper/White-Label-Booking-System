a4f877f385bfad253c2fd29e13005a81bf7c7036
SandyWyper <sandywyper@gmail.com>
2025-08-18 10:40:15 +0100

    tailwind & DaisyUI installed

--- white_label_booking/urls.py
+++ white_label_booking/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -25,3 +26,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('bookings/', include("bookings.urls")),
 ]
+
+if settings.DEBUG:
+    # Include django_browser_reload URLs only in DEBUG mode
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
